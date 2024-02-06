51d6b431e277284d8847cedd6f8a2c5c94b2d928
mayu <mayu.k0219sun@gmail.com>
2024-02-06 16:43:37 +0900

    App.tsx追加

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import "../static/styles.css";
+ 
+export const App: React.FC = () => {
+    const [data, setData] = useState<string>('');
+
+    useEffect(() => {
+        fetch('http://localhost:3001')
+        .then((response) => response.text())
+        .then((responseData) => setData(responseData))
+        .catch((error) => console.error('Error:', error));
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Hello frm React!</h1>
+            <p>Server Response: {data} </p>
+        </div>
+    );
+};
