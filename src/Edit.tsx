d54ddfe8acbcb0de4fe11e07fd3a0cd50d858784
mayu <mayu.k0219sun@gmail.com>
2024-02-21 23:17:09 +0000

    Editコンポーネント実装

--- src/Edit.tsx
+++ src/Edit.tsx
@@ -1,3 +1,4 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import React from 'react';
 import './Edit.css';
 type DataTypes = {
@@ -9,18 +10,47 @@ type EditProps = {
     show: boolean;
     setShow: React.Dispatch<React.SetStateAction<boolean>>;
 }
+type DataType = {
+    id: string;
+    editCategoryname: string;
+   };
 
 export function Edit({editData, show, setShow}: EditProps){
-        if(show){
-            return(
-                <div id="overlay">
-                    <div id="content">
-                        {editData.categoryname}
-                        <button onClick={() => setShow(false)}>Cancel</button>
-                    </div>
-                </div>
-                );
-        }else{
-            return null;
+
+    const { 
+        register, 
+        handleSubmit 
+    } = useForm<DataType>();
+    //update処理
+    const updateData = async (data: DataType) => {
+        try{
+            const response = await fetch("http://localhost:3001/update", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+        } catch(error) {
+            console.log(error);
         }
+        setShow(false);
+    };
+
+    if(show){
+        return(
+            <div id="overlay">
+                <div id="content">
+                    <form onSubmit={handleSubmit(updateData)}>
+                        <input type="hidden" {...register("id",{ value: editData.id })}/>
+                        <input type="text" {...register("editCategoryname")}/>
+                        <button type="submit">submit</button>
+                    </form>
+                    <button onClick={() => setShow(false)}>Cancel</button>
+                </div>
+            </div>
+            );
+    }else{
+        return null;
     }
+}
