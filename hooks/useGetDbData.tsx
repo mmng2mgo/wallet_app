f586d1e988288bcb595d628d5b286667f15724d1
mayu <mayu.k0219sun@gmail.com>
2024-02-25 09:00:52 +0000

    get処理をカスタムフック化

---
+++ hooks/useGetDbData.tsx
@@ -0,0 +1,20 @@
+//return data
+import { useEffect } from 'react';
+
+interface Props<T>{
+    url: string;
+    datas: T[];
+    setDatas: React.Dispatch<React.SetStateAction<T[]>>;
+}
+//汎用性を持たせるためにジェネリクスインターフェースを引数にした
+export const useGetDbData = <T,>({ url, datas, setDatas}: Props<T>) => {
+    useEffect(() => {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data: T[]) => {
+            setDatas(data);
+        })
+        .catch((error) => console.error('Error:', error));
+    }, [datas]);
+    return [datas];
+}
