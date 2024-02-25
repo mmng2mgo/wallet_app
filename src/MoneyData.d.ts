41fde745cc8257f5ebb0c25eaadde4e685ed399c
mayu <mayu.k0219sun@gmail.com>
2024-02-25 09:02:18 +0000

    インターフェイスの追加

--- src/MoneyData.d.ts
+++ src/MoneyData.d.ts
@@ -1,10 +1,15 @@
 export interface MoneyDataTypes {
     id: string;
     categoryname: string;
+    editCategoryname: string;
 }
 
 export interface MoneyDataType {
     id: string;
     categoryname: string;
-    editCategoryName: string;
+    editCategoryname: string;
+}
+export interface EditMoneyDataType {
+    id: string;
+    editCategoryname: string;
 }
