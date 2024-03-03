9288ef2dc2388e8f4240e921f313978bc38ca444
mayu <mayu.k0219sun@gmail.com>
2024-03-03 05:47:14 +0000

    Formに引数をとる

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Form.css'
-export function Form(){
+import { MoneyDataTypes } from './MoneyData';
+//Formの引数がincomesとspendingの場合で異なるもしくはプロバイダの変更、categoryのセレクトボックスコンポーネント
+
+interface FormProps {
+    dataList: MoneyDataTypes[];
+}
+
+export function Form(props: FormProps) {
+    const { dataList } = props;
+
     return (
         <div className="container">
             <form>
@@ -12,7 +21,6 @@ export function Form(){
                 </div>
                 <div className='inputrow'>
                     <label>
-                    {/* カテゴリーのvalueはpropsで受け取る */}
                         カテゴリー:
                         <input name="category"/>
                     </label>
@@ -31,6 +39,55 @@ export function Form(){
                 </div>
                 <input type="submit" value="保存"/>
             </form>
+            <tbody>
+                {dataList.map((data) => (
+                    <tr key={data.id}>
+                        <td>{data.categoryname}</td>
+                    </tr>
+                ))}
+            </tbody>
         </div>
     );
 }
+
+// export function Form(dataList: MoneyDataTypes[]) {
+//     return (
+//         <div className="container">
+//             <form>
+//                 <div className='inputrow'>
+//                     <label>
+//                         金額:
+//                         <input name="amount"/>
+//                     </label>
+//                 </div>
+//                 <div className='inputrow'>
+//                     <label>
+//                     {/* カテゴリーのvalueはpropsで受け取る */}
+//                         カテゴリー:
+//                         <input name="category"/>
+//                     </label>
+//                 </div>
+//                 <div className='inputrow'>
+//                     <label>
+//                         備考:
+//                         <input name="memo"/>
+//                     </label>
+//                 </div>
+//                 <div className='inputrow'>
+//                     <label>
+//                         日付:
+//                         <input name="date"/>
+//                     </label>
+//                 </div>
+//                 <input type="submit" value="保存"/>
+//             </form>
+//             <tbody>
+//                     {dataList.map((data) => (
+//                         <tr key={data.id}>
+//                             <td>{data.categoryname}</td>
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//         </div>
+//     );
+// }
--- src/Incomes.tsx
+++ src/Incomes.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from './Form';
+import { useGetDbData } from '../hooks/useGetDbData';
+import { MoneyDataTypes } from './MoneyData';
 
 export const Incomes = () => {
+    const [incomesCategories, setIncomesCategories] = useState<MoneyDataTypes[]>([]);
+    const [dataList] = useGetDbData<MoneyDataTypes>({url:'http://localhost:3001/incomes', datas: incomesCategories, setDatas:　setIncomesCategories});
+    
     return (
         <div>
             <h1>収入</h1>
-            <Form />
+            {dataList.length > 0 && <Form dataList={dataList}/>}
         </div>
     );
 }
