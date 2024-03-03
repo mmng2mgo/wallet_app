957918b975b6d9cfb86a60aa71ec23e59492e9f9
mayu <mayu.k0219sun@gmail.com>
2024-03-03 07:16:30 +0000

    selectBoxにincomes_categoryテーブル表示

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './Form.css'
 import { MoneyDataTypes } from './MoneyData';
-//Formの引数がincomesとspendingの場合で異なるもしくはプロバイダの変更、categoryのセレクトボックスコンポーネント
+//引数にurlを追加。usePostDbDataを用いてdbを操作できるようにする
 
 interface FormProps {
     dataList: MoneyDataTypes[];
@@ -9,6 +10,7 @@ interface FormProps {
 
 export function Form(props: FormProps) {
     const { dataList } = props;
+    const { register, handleSubmit } = useForm();
 
     return (
         <div className="container">
@@ -37,57 +39,17 @@ export function Form(props: FormProps) {
                         <input name="date"/>
                     </label>
                 </div>
+                <select {...register("incomesCategoryId")}>
+                    {dataList.map((data) => {
+                        return (
+                            <option key={data.id} value={data.id}>
+                                {data.categoryname}
+                            </option>
+                        );
+                    })}
+                </select>
                 <input type="submit" value="保存"/>
             </form>
-            <tbody>
-                {dataList.map((data) => (
-                    <tr key={data.id}>
-                        <td>{data.categoryname}</td>
-                    </tr>
-                ))}
-            </tbody>
         </div>
     );
 }
-
-// export function Form(dataList: MoneyDataTypes[]) {
-//     return (
-//         <div className="container">
-//             <form>
-//                 <div className='inputrow'>
-//                     <label>
-//                         金額:
-//                         <input name="amount"/>
-//                     </label>
-//                 </div>
-//                 <div className='inputrow'>
-//                     <label>
-//                     {/* カテゴリーのvalueはpropsで受け取る */}
-//                         カテゴリー:
-//                         <input name="category"/>
-//                     </label>
-//                 </div>
-//                 <div className='inputrow'>
-//                     <label>
-//                         備考:
-//                         <input name="memo"/>
-//                     </label>
-//                 </div>
-//                 <div className='inputrow'>
-//                     <label>
-//                         日付:
-//                         <input name="date"/>
-//                     </label>
-//                 </div>
-//                 <input type="submit" value="保存"/>
-//             </form>
-//             <tbody>
-//                     {dataList.map((data) => (
-//                         <tr key={data.id}>
-//                             <td>{data.categoryname}</td>
-//                         </tr>
-//                     ))}
-//                 </tbody>
-//         </div>
-//     );
-// }
